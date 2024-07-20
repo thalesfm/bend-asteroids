3fce1eda65aff1dd786342faa1a30f75d6be45f0
Thales Magalhães <thls.mglhs@gmail.com>
2024-07-20 11:31:43 -0300

    Run an HVM program directly

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,94 @@
-use bend::{compile_book, load_file_to_book, run_book, CompileOpts, RunOpts};
-use bend::fun::{Book, Name, Term};
-use bend::diagnostics::{Diagnostics, DiagnosticsConfig};
+// use bend::{compile_book, load_file_to_book, run_book, CompileOpts, RunOpts};
+// use bend::fun::{Book, Name, Term};
+// use bend::diagnostics::{Diagnostics, DiagnosticsConfig};
+use hvm::hvm;
+use ::hvm::ast;
 use std::path::Path;
 
-fn load_book(path: &Path) -> Result<Book, Diagnostics> {
-    let mut book = load_file_to_book(path)?;
-    book.entrypoint = Some(Name::new("init"));
-    Ok(book)
+fn run(book: &hvm::Book) {
+    let net = hvm::GNet::new(1 << 29, 1 << 29);
+    let mut tm = hvm::TMem::new(0, 1);
+
+    /*
+    let port: hvm::Port = hvm::Port::new(0, hvm::ROOT.get_val() + 1);
+    tmem.rbag.push_redex(hvm::Pair::new(hvm::Port::new(hvm::REF, main_id as u32), port));
+    gnet.vars_create(port.get_val() as usize, hvm::NONE);
+    tmem.evaluator(&gnet, &book);
+
+    tmem.rbag.push_redex(hvm::Pair::new(hvm::Port::new(hvm::REF, port.get_val()), hvm::ROOT));
+    gnet.vars_create(hvm::ROOT.get_val() as usize, hvm::NONE);
+    tmem.evaluator(&gnet, &book);
+    */
+
+    /*
+    let init_id = book.defs.iter().position(|def| def.name == "init_").unwrap();
+    let update_id = book.defs.iter().position(|def| def.name == "update").unwrap();
+
+    let var1 = hvm::Port(0xFFFFFFE8);
+    let var2 = hvm::Port(0xFFFFFFF0);
+    let root = hvm::ROOT;
+
+    assert!(net.is_vars_free(var1.get_val() as usize));
+    assert!(net.is_vars_free(var2.get_val() as usize));
+
+    net.vars_create(var1.get_val() as usize, hvm::NONE);
+    net.vars_create(var2.get_val() as usize, hvm::NONE);
+    net.vars_create(root.get_val() as usize, hvm::NONE);
+
+    // Reduce @init_ ~ var1
+    let redex = hvm::Pair::new(hvm::Port::new(hvm::REF, init_id as u32), var1);
+    tmem.rbag.push_redex(redex);
+    tmem.evaluator(&net, &book);
+
+    // Reduce @update ~ (var1 root)
+    hvm::Port::new(hvm::CON, )
+    let redex = hvm::Pair::new(root, var1);
+    tmem.rbag.push_redex(redex);
+    tmem.evaluator(&net, &book);
+    */
+
+    // Allocates needed nodes and vars.
+    // let got = tmem.get_resources(&net, 0, 0, 0);
+    // assert!(got);
+
+    // Checks availability
+    /*
+    if net.node_load(a.get_val() as usize).0 == 0 || net.node_load(b.get_val() as usize).0 == 0 {
+      panic!()
+    }
+    */
+
+    // Stores new vars.
+    // net.vars_create(tmem.vloc[0], hvm::NONE);
+    // net.vars_create(tmem.vloc[1], hvm::NONE);
+    // etc.
+
+    // Stores new nodes.
+    // net.node_create(tmem.nloc[0], Pair::new(Port::new(VAR, self.vloc[0] as u32), Port::new(VAR, self.vloc[1] as u32)));
+    // net.node_create(tmem.nloc[1], Pair::new(Port::new(VAR, self.vloc[2] as u32), Port::new(VAR, self.vloc[3] as u32)));
+    // etc.
+
+    // Links.
+    // tmem.link_pair(&net, Pair::new(Port::new(b.get_tag(), self.nloc[0] as u32), a1));
+    // tmem.link_pair(&net, Pair::new(Port::new(b.get_tag(), self.nloc[1] as u32), a2));
+    // etc.
+
+    let main_id = book.defs.iter().position(|def| def.name == "main").unwrap();
+    tm.rbag.push_redex(hvm::Pair::new(hvm::Port::new(hvm::REF, main_id as u32), hvm::ROOT));
+    net.vars_create(hvm::ROOT.get_val() as usize, hvm::NONE);
+    // tm.link_pair(&net, hvm::Pair::new(hvm::Port::new(hvm::REF, main_id as u32), hvm::ROOT));
+    tm.evaluator(&net, &book);
+
+    if let Some(net) = ast::Net::readback(&net, &book) {
+        println!("Result: {}", net.show());
+    } else {
+        panic!("Readback failed");
+    }
 }
 
 fn main() {
-    /*
-    // run_opts: CliRunOpts
-    let linear = false; // ???
-    let print_stats = false;
-
-    // runArgs: RunArgs
-    let pretty = true;
-    // let run_opts = run_opts;
-    // let comp_opts = ???
-    // let warn_opts = ???
-    let path = Path::new("./app.bend");
-    let arguments: Option<Vec<bend::fun::Term>> = None;
-
-    // cli: Cli
-    // let mode = Mode::Run(run_args);
-    let verbose = true;
-    // let hvm_bin: Option<String> = None;
-    // let entrypoint: Option<String> = Some("_init".to_string());
-
-    let hvm_bin = "hvm".to_string();
-    let run_cmd = "run";
-
-    let mut book = load_book(&path).unwrap(); // ? not working for some reason
-    let run_opts = RunOpts { linear_readback: linear, pretty, hvm_path: hvm_bin };
-    let compile_opts = CompileOpts::default();
-    let diagnostics_cfg = DiagnosticsConfig::default();
-
-    // compile_book(&mut book, compile_opts.clone(), diagnostics_cfg, arguments.clone()).unwrap();
-
-    match run_book(book, run_opts, compile_opts, diagnostics_cfg, arguments, run_cmd).unwrap() {
-        Some((term, stats, diags)) => {
-            println!("Success! Result:");
-            println!("{}", term.display_pretty(0));
-        }
-        None => {
-            println!("Failed :(")
-        }
-    };
-    */
-
-    let path = Path::new("./init.bend");
-    let mut book = load_book(&path).unwrap();
-    let state: Term;
-    // book.entrypoint = Some(Name::new("foo"));
-    let result = run_book(
-        book,
-        RunOpts::default(),
-        CompileOpts::default(),
-        DiagnosticsConfig::default(),
-        None,
-        "run");
-    match result.unwrap() {
-        Some((term, stats, diags)) => {
-            println!("init() -> state: {}", term.display_pretty(0));
-            state = term;
-        }
-        _ => panic!()
-    };
-
-    let path = Path::new("./update.bend");
-    let book = load_book(&path).unwrap();
-    let args = vec![state];
-    let result = run_book(book, RunOpts::default(), CompileOpts::default(), DiagnosticsConfig::default(), Some(args), "run");
-    let (term, _, _) = result.unwrap().unwrap();
-    println!("update(state) -> {}", term.display_pretty(0));
-    println!("Hello, world!");
+    let path = "app.hvm";
+    let code = std::fs::read_to_string(path).expect("Unable to read file");
+    let book = ast::Book::parse(&code).unwrap().build();
+    run(&book);
 }
